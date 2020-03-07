bf447fb9eda9cdaeba9f3105bd3d879ecf5c050f
saurabh gupta <saurabhguptarock119@gmail.com>
2020-03-07 16:35:56 +0530

    .

--- main.ts
+++ main.ts
@@ -1,32 +1,70 @@
-import { app, BrowserWindow, screen } from "electron";
+import { app, BrowserWindow, dialog, nativeImage } from "electron";
 import * as path from "path";
 import * as url from "url";
+import { autoUpdater } from "electron-updater";
 
 let win: BrowserWindow = null;
+const iconPath = nativeImage.createFromPath(
+  path.join(__dirname, "/src/assets/icons/icon.png")
+);
 const args = process.argv.slice(1),
   serve = args.some(val => val === "--serve");
 
-function createWindow(): BrowserWindow {
-  const electronScreen = screen;
-  const size = electronScreen.getPrimaryDisplay().workAreaSize;
+autoUpdater.on("checking-for-update", () => {
+  dialog.showMessageBox({
+    title: "Ds Recorder",
+    message: "Checking for Update",
+    icon: iconPath
+  });
+});
 
-  // Create the browser window.
+autoUpdater.on("update-available", info => {
+  dialog.showMessageBox({
+    title: "Ds Recorder",
+    message: `A new update is ready to install
+       Version ${info.version} is downloaded and will be automatically installed.`,
+    buttons: ["OK"],
+    icon: iconPath
+  });
+});
+
+autoUpdater.on("update-not-available", () => {
+  dialog.showMessageBox({
+    title: "Ds Recorder",
+    message: "There is no update Currently",
+    buttons: ["OK"],
+    icon: iconPath
+  });
+});
+
+autoUpdater.on("update-downloaded", info => {
+  autoUpdater.quitAndInstall();
+});
+
+autoUpdater.on("error", error => {
+  dialog.showMessageBox({
+    title: "Ds Recorder",
+    message: error,
+    buttons: ["OK"],
+    icon: iconPath
+  });
+});
+
+function createWindow(): BrowserWindow {
   win = new BrowserWindow({
     x: 0,
     y: 0,
-    width: size.width,
-    height: size.height,
+    icon: path.join(__dirname, "/src/assets/icons/icon.ico"),
+    show: false,
     webPreferences: {
       nodeIntegration: true,
       allowRunningInsecureContent: serve ? true : false
     }
   });
-
   if (serve) {
     require("electron-reload")(__dirname, {
       electron: require(`${__dirname}/node_modules/electron`)
     });
-    win.maximize();
     win.loadURL("http://localhost:4200");
   } else {
     win.loadURL(
@@ -41,41 +79,27 @@ function createWindow(): BrowserWindow {
   if (serve) {
     win.webContents.openDevTools();
   }
-
-  // Emitted when the window is closed.
   win.on("closed", () => {
-    // Dereference the window object, usually you would store window
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null;
+    app.quit();
   });
-
+  win.once("ready-to-show", () => {
+    win.show();
+  });
+  win.maximize();
   return win;
 }
 
 try {
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
   app.on("ready", createWindow);
-
-  // Quit when all windows are closed.
   app.on("window-all-closed", () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== "darwin") {
       app.quit();
     }
   });
-
   app.on("activate", () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (win === null) {
       createWindow();
     }
   });
-} catch (e) {
-  // Catch Error
-  // throw e;
-}
+} catch (e) {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from "@angular/core";
+import { desktopCapturer, remote } from "electron";
+import { from } from "rxjs";
 
+const { dialog, Menu } = remote;
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
